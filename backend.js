7fab33c94e5169405d4985b71e3d0d4bf565998c
Sebastian Glaser <anx@ulzq.de>
2020-04-22 15:50:01 +0200

    kommentare

--- backend.js
+++ backend.js
@@ -1,4 +1,29 @@
 
+/*
+
+  Problem:
+    -- Böse Hacker können die Datenbank klauen
+    => Passwörter in der Datenbank
+    => Hacker kann die Passwörter für andere Seiten benutzen
+
+  Lösung:
+    -- Passwörter müssen gehasht werden (quasi verschlüsselt)
+    => Hacker kann die Passwörter nicht für andere Server benutzen
+
+  Was ist denn so ein Hash:
+    -- Der hash ist das Ergebnis einer Hashfunktion
+       z.B.: sha1, sh512, md5, fnv32
+       fnv32('hallo') => 12698376921
+       fnv32('hallö') => 12938792187
+       fnv32('hallo') == fnv32('hallo')
+
+  Aber was kann ich tun damit ich mich jedes mal neu anmelden muss?
+    -- Das Backend gibt dem Frontend nach erfolgreicher Anmeldung ein Token
+       --> 9876989a8769a876e9a876e.a87e69a87e.ae0897a097ea
+       --> Frontend schickt das Token nun für alle Anfragen mit
+       --> Backend kann mit [verify] das token checken und daten herauslesen
+*/
+
 const express = require('express');
 const app     = express();
 
@@ -20,12 +45,10 @@ app.get('/login',(req,res)=> {
     pass,            // klartext passwort von benutzer
     User[name].pass, // hashed passwort aus db
     function(err, result) {
-      const token = jwt.sign({name},secret,{expiresIn:'5s'});
+      const token = result ? jwt.sign({name},secret,{expiresIn:'5s'}) : null;
       console.log('login for', name, result, token );
-      return res.json({
-        success:result,
-        token
-      });
+      //
+      return res.json({ success:result, token });
   });
 
 });
