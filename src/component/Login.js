2d444b0953545d4fe939fb62c3fb93d4aca2d45e
Sebastian Glaser <anx@ulzq.de>
2020-04-23 14:01:02 +0200

    add: remember me funktion

--- src/component/Login.js
+++ src/component/Login.js
@@ -10,8 +10,13 @@ export default function Login() {
   const [state,setState] = React.useState({
     name:'', pass:'', login:false
   });
-  const { name, pass, login, registered, token } = state;
-  const change = e => setState({ ...state, [e.target.name]: e.target.value });
+  const { name, pass, login, remember, token } = state;
+
+  const change = e =>
+    setState({ ...state, [e.target.name]: e.target.value });
+
+  const changeCheckbox = e =>
+    setState({ ...state, remember: e.target.checked });
 
   const submit = e => {
     e.preventDefault();
@@ -19,7 +24,9 @@ export default function Login() {
     .then( response => response.json() )
     .then( result   => {
       window.AUTH_TOKEN = result.token;
-      localStorage.setItem('jwt',result.token);
+      if ( remember )
+           localStorage.setItem('jwt',result.token);
+      else localStorage.removeItem('jwt');
       setState({
         ...state,
         login:result.success,
@@ -32,6 +39,7 @@ export default function Login() {
   return ( <>
     <p><input name="name" value={name} onChange={change}/></p>
     <p><input name="pass" value={pass} onChange={change}/></p>
+    <p><input type="checkbox" name="remember" checked={remember} onChange={changeCheckbox}/> Remember Me</p>
     <p><button onClick={submit}>Login</button></p>
     <p>Noch kein Kunde? <Link to='/register'>Hier Registrieren</Link></p>
   </> );
